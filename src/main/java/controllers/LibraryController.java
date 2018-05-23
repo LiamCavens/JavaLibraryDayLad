150e43707e8e5340b5b260fe84aeb3c8700bede5
Liam Cavens <liamcavens@googlemail.com>
2018-05-23 15:22:26 +0100

    updated pom and hibernate.cfg

--- src/main/java/controllers/LibraryController.java
+++ src/main/java/controllers/LibraryController.java
@@ -1,7 +1,15 @@
 package controllers;
 
+import db.Seed;
+
+import static spark.Spark.get;
+import static spark.Spark.modelAndView;
+import static spark.SparkBase.staticFileLocation;
+
 public class LibraryController {
 
-    public static void main(String[] args)
+    public static void main(String[] args){
+        Seed.seedData();
+
     }
 }
